19aca025314b3281fd4ff99fa1ff59251acaee5f
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-03-29 11:06:16 +0100

    changed to return object with result.rows as value. Have amended tests to check for this too

--- models/topic.model.js
+++ models/topic.model.js
@@ -3,5 +3,5 @@ const db = require("../db/connection");
 exports.selectTopics = async () => {
     const result = await db.query(`SELECT * FROM topics;`)
     // console.log("===========>", Object.keys(result.rows[0]))
-    return result.rows;
+    return { topics: result.rows };
 }
