e03211f1d6e525015848ef4ce435ceb798bd2c75
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-03-30 12:32:00 +0100

    testing /api/users endpoint and setting up functions to deal with this. Also added model and controller files for users

--- models/model-connection.js
+++ models/model-connection.js
@@ -1,4 +1,5 @@
 const { fetchTopics } = require("./topic.model")
 const { fetchArticle, updateArticle } = require("./article.model")
+const { fetchUsers } = require("./users.model")
 
-module.exports = { fetchTopics, fetchArticle, updateArticle }
+module.exports = { fetchTopics, fetchArticle, updateArticle, fetchUsers }
