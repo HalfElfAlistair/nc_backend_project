effe5904a16d4a9cb0e8c035f7055b3c34fbcf32
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-03-31 10:23:04 +0100

    testing and refactoring code for GET /api/articles/:article_id so that is now produces a comment count property as well

--- models/article.model.js
+++ models/article.model.js
@@ -3,11 +3,23 @@ const db = require("../db/connection");
 exports.fetchArticle = async (id) => {
     const queryStr = `SELECT * FROM articles WHERE article_id = $1;`
     const result = await db.query(queryStr, [id])
+
     if (result.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "article not found"})
     }
+    const comments = await addCommentCount(id)
+    result.rows[0]["comment_count"] = comments.length;
     return result.rows[0];
 }
+
+// exports.fetchArticle = async (id) => {
+//     const queryStr = `SELECT * FROM articles WHERE article_id = $1;`
+//     const result = await db.query(queryStr, [id])
+//     if (result.rows.length === 0) {
+//         return Promise.reject({ status: 404, msg: "article not found"})
+//     }
+//     return result.rows[0];
+// }
 
 exports.updateArticle = async (id, voteChange) => {
     const queryStr = `UPDATE articles
@@ -18,9 +30,13 @@ exports.updateArticle = async (id, voteChange) => {
 
     const result = await db.query(queryStr, [voteChange, id]);
 
-    // if (result.rows.length === 0) {
-    //     return Promise.reject({ status: 404, msg: "article not found"})
-    // }
-
     return result.rows[0];
+}
+
+const addCommentCount = async (id) => {
+    const commentsById = `SELECT * 
+    FROM comments 
+    WHERE article_id = $1;`
+    const commentsData = await db.query(commentsById, [id]);
+    return commentsData.rows
 }
