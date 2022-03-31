da69b99e24b16a26dfaa70912b90ac5936845e24
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-03-31 10:09:55 +0100

    reset main, accidentally commited changes to wrong branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,7 +38,7 @@ describe("GET /api/topics", () => {
 })
 
 describe("GET /api/articles/:article_id", () => {
-    xtest("Responds with an article object, which should match a list of provided properties.", () => {
+    test("Responds with an article object, which should match a list of provided properties.", () => {
         return request(app)
             .get("/api/articles/1")
             .expect(200)
@@ -159,63 +159,6 @@ describe("GET /api/users", () => {
             .expect(404)
             .then(({ body }) => {
                 expect(body.msg).toBe("path not found")
-            }) 
-    })
-})
-
-describe("GET /api/articles/:article_id (comment count)", () => {
-    test("Responds with the article object, now including the total count of comments with this article_id", () => {
-        return request(app)
-            .get("/api/articles/1")
-            .expect(200)
-            .then((res) => {
-                const article = res.body.article;
-                const output = {
-                    author: "butter_bridge",
-                    title: "Living in the shadow of a great man",
-                    article_id: 1,
-                    body: "I find this existence challenging",
-                    topic: "mitch",
-                    created_at: "2020-07-09T20:11:00.000Z",
-                    votes: 100,
-                    comment_count: 11
-                }
-                expect(article).toEqual(output);
-            })
-    })
-    test("Responds with the article object, and comment count of 0 when there are none matching the article_id", () => {
-        return request(app)
-            .get("/api/articles/4")
-            .expect(200)
-            .then((res) => {
-                const article = res.body.article;
-                const output = {
-                    author: "rogersop",
-                    title: "Student SUES Mitch!",
-                    article_id: 4,
-                    body: "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
-                    topic: "mitch",
-                    created_at: "2020-05-06T01:14:00.000Z",
-                    votes: 0,
-                    comment_count: 0
-                }
-                expect(article).toEqual(output);
-            })
-    })
-    test("returns '404 - path not found' if id doesn't exist", () => {
-        return request(app)
-            .get("/api/articles/1000")
-            .expect(404)
-            .then(({ body }) => {
-                expect(body.msg).toBe("article not found")
-            }) 
-    })
-    test("returns '400 - bad request' if id type is wrong", () => {
-        return request(app)
-            .get("/api/articles/one")
-            .expect(400)
-            .then(({ body }) => {
-                expect(body.msg).toBe("bad request")
             }) 
     })
 })
--- models/article.model.js
+++ models/article.model.js
@@ -6,10 +6,6 @@ exports.fetchArticle = async (id) => {
     if (result.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "article not found"})
     }
-
-    const commentsArray = await addCommentCount(id)
-    result.rows[0]["comment_count"] = commentsArray.length;
-
     return result.rows[0];
 }
 
@@ -22,14 +18,9 @@ exports.updateArticle = async (id, voteChange) => {
 
     const result = await db.query(queryStr, [voteChange, id]);
 
-    return result.rows[0];
-}
+    // if (result.rows.length === 0) {
+    //     return Promise.reject({ status: 404, msg: "article not found"})
+    // }
 
-const addCommentCount = async (id) => {
-    // takes id argument and uses it to match any comments with the same article_id. Returns array
-    const commentsById = `SELECT * 
-    FROM comments 
-    WHERE article_id = $1;`
-    const commentsData = await db.query(commentsById, [id]);
-    return commentsData.rows
+    return result.rows[0];
 }
