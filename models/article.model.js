282df69edd2589632bd80f9c60f1ceae2c80d7fd
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-03-31 14:46:24 +0100

    further testing and completing of GET /api/articles endpoint

--- models/article.model.js
+++ models/article.model.js
@@ -38,11 +38,10 @@ exports.fetchArticles = async () => {
     CAST(COUNT(comments.comment_id) AS int) AS comment_count
     FROM articles
     LEFT JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id;`
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;`
 
     const articlesData = await db.query(queryStr)
-
-    console.log(articlesData.rows)
 
     return articlesData.rows;
 }
