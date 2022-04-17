0eab2493981e98d3817ce3ae08f5b050fab43e2e
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-04-17 15:52:35 +0100

    adding listen.js file and ammending connection.js and package.json in order to host with heroku

---
+++ listen.js
@@ -0,0 +1,12 @@
+// const PORT = process.env.PORT || 9090;
+// const app = require('./app');
+
+// app.listen(PORT, (err) => {
+//   if (err) throw err;
+//   console.log(`Listening on port: ${PORT}...`);
+// });
+
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
