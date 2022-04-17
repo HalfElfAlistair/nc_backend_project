56fb5e201f44937a72aede67c99bcad8e3c1d2f9
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-04-17 20:25:37 +0100

    hosting on heroku

--- controllers/controller-connection.js
+++ controllers/controller-connection.js
@@ -1,6 +1,6 @@
-const { getTopics } = require("./topic.controller")
-const { getArticle, getArticles, patchArticle, getComments, postComment } = require("./article.controller")
-const { getUsers } = require("./users.controller")
-const { deleteComment } = require("./comment.controller")
+const { getTopics } = require("./topic.controller");
+const { getArticle, getArticles, patchArticle, getComments, postComment } = require("./article.controller");
+const { getUsers } = require("./users.controller");
+const { deleteComment } = require("./comment.controller");
 
 module.exports = { getTopics, getArticle, getArticles, patchArticle, getUsers, getComments, postComment, deleteComment }
--- models/model-connection.js
+++ models/model-connection.js
@@ -1,6 +1,6 @@
-const { fetchTopics } = require("./topic.model")
-const { fetchArticle, fetchArticles, updateArticle, fetchComments, addComment } = require("./article.model")
-const { fetchUsers } = require("./users.model")
-const { removeComment } = require("./comment.model")
+const { fetchTopics } = require("./topic.model");
+const { fetchArticle, fetchArticles, updateArticle, fetchComments, addComment } = require("./article.model");
+const { fetchUsers } = require("./users.model");
+const { removeComment } = require("./comment.model");
 
 module.exports = { fetchTopics, fetchArticle, fetchArticles, updateArticle, fetchComments, fetchUsers, addComment, removeComment }
