f5cfca717489d7c5def70722ec540cc12bce8dbf
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-04-19 11:11:09 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const { getTopics, getArticle, patchArticle, getUsers, getArticles, getComments, postComment, deleteComment } = require("./controllers/controller-connection")
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
